93c7a77081539f431c08d102cfdabd29f24c74d1
syueer <syue531@hotmail.com>
2022-06-25 20:48:34 +1000

    bootstrap validation

--- js/index.js
+++ js/index.js
@@ -21,8 +21,19 @@
 })()
 
 let tasks = new TaskManager()
+tasks.render()
 // console.log(tasks)
+const taskHtml = createTaskHtml("takingout trash", 'please tak out trash end of the day', 'Yue', '26/07/2022', 'todo')
+// console.log(taskHtml)
+const validFormFieldInput = (data) => {
+  const newTaskNameInput = document.querySelector('#validationTaskName');
+  const newTaskDescriptionInput = document.querySelector('#validationTaskDescription');
+  const newNameInput = document.querySelector('#validationName');
 
+  const name = newTaskNameInput.value;
+  console.log("name:  " + name);
+
+}
 
 
 // tasks.addTask('shop', 'shopping things', 'yue', 25 / 03 / 2020)
@@ -31,12 +42,12 @@ let tasks = new TaskManager()
 // console.log(tasks)
 // console.log(submit)
 
-let el = document.querySelector(".submit")
-el.addEventListener("click", () => {
-  tasks.addTask()
-});
+// let el = document.querySelector(".submit")
+// el.addEventListener("click", () => {
+//   tasks.addTask()
+// });
 
-console.log(el)
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-}
+// console.log(el)
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,31 @@
+
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+  const html = `
+  <div class="card mb-3">
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <h6 class="card-title mb-2">Assigned: ${assignedTo}</h6>
+      <p class="card-text">Description: ${description}</p>
+      <div>
+        <i class="fa fa-clock"></i>
+        <span>${dueDate}</span>
+      </div>
+      <div>${status}</div>
+      <div>
+        <a href="#" class="card-link"
+          ><i class="fa-solid fa-pen-to-square"></i
+        ></a>
+        <a href="#" class="card-link">
+          <i class="fa fa-trash-can"></i
+        ></a>
+      </div>
+    </div>
+  </div>`
+  return html
+}
+
+
+
 class TaskManager {
   constructor(currentId = 0) {
     this.currentId = currentId
@@ -7,5 +35,15 @@ class TaskManager {
   addTask(name, description, assignedTo, dueDate, status = 'todo') {
     this.currentId++
     this.tasks.push([this.currentId, name, description, assignedTo, dueDate, status])
+  }
+
+  render() {
+    let tasksHtmlList = []
+    this.tasks.map(task => {
+      let currentTask = task;
+      let date = new Date(task.dueDate)
+      date.getDate()
+      // let formattedDate = 
+    })
   }
 }
