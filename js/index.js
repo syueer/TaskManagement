93b0d8ea3c6156fe2c8776b73268f74270b1b28f
syueer <syue531@hotmail.com>
2022-06-26 21:29:28 +1000

    add card

--- js/index.js
+++ js/index.js
@@ -1,39 +1,33 @@
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
-
 let tasks = new TaskManager()
-tasks.render()
+// tasks.render()
 // console.log(tasks)
-const taskHtml = createTaskHtml("takingout trash", 'please tak out trash end of the day', 'Yue', '26/07/2022', 'todo')
-// console.log(taskHtml)
-const validFormFieldInput = (data) => {
-  const newTaskNameInput = document.querySelector('#validationTaskName');
-  const newTaskDescriptionInput = document.querySelector('#validationTaskDescription');
-  const newNameInput = document.querySelector('#validationName');
+// const taskHtml = createTaskHtml("takingout trash", 'please tak out trash end of the day', 'Yue', '26/07/2022', 'todo')
 
-  const name = newTaskNameInput.value;
-  console.log("name:  " + name);
+const submitButton = document.querySelector('#submitButton')
+document.querySelector('#alertInfo').style.display = 'none'
+submitButton.onclick = (e) => {
+  e.preventDefault()
 
+  let newTaskName = document.querySelector('#taskName');
+  let newTaskDescription = document.querySelector('#taskDescription');
+  let newAssignedName = document.querySelector('#assignedName');
+  let newDueDate = document.querySelector('#dueDate');
+  let newStatus = document.querySelector('#status');
+  console.log(newTaskName.value)
+  if (newTaskName.value === '') {
+    document.querySelector('#alertInfo').style.display = 'block'
+    document.querySelector('#alertInfo').innerText = 'Please input taskName'
+  } else if (newTaskDescription.value === '') {
+    document.querySelector('#alertInfo').style.display = 'block'
+    document.querySelector('#alertInfo').innerText = 'Please input taskDescription'
+  } else if (newAssignedName.value === '') {
+    document.querySelector('#alertInfo').style.display = 'block'
+    document.querySelector('#alertInfo').innerText = 'Please input AssignedName'
+  }
 }
+
+
 
 
 // tasks.addTask('shop', 'shopping things', 'yue', 25 / 03 / 2020)
